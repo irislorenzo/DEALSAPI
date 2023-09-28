c509919225c676f62d8041e12dbe4dfe1a8ab1d0
jeugenioDHP <jeugenio@discoveryparks.com.au>
2023-09-28 11:44:34 +0800

    Add Deal Negative Validation Tests
    - Update DealTemplate name to DealPositiveValidation
    - Add DealNegativeValidation
    - Add 12 Negative Test under DealNegative.feature
    - Update Scenario name

--- apitests/src/test/java/deals/DealRunnerTest.java
+++ apitests/src/test/java/deals/DealRunnerTest.java
@@ -3,10 +3,14 @@ package deals;
 import com.intuit.karate.junit5.Karate;
 
 public class DealRunnerTest {
-
+	
 	@Karate.Test
-    Karate DealTemplate() {
+    Karate DealPositiveValidation() {
         return Karate.run("DealPositive").relativeTo(getClass());
     }
 
+	@Karate.Test
+    Karate DealNegativeValidation() {
+        return Karate.run("DealNegative").relativeTo(getClass());
+    }
 }
